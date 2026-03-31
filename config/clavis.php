e779a2b8f596d1d00d279b533a8726da5c3dd321
Siarhei Kulich <siarhei.kulich@gmail.com>
2026-03-31 14:03:30 +0200

    Add documentation for `CLAVIS_HASH` configuration in `clavis.php`

--- config/clavis.php
+++ config/clavis.php
@@ -1,5 +1,19 @@
 <?php
 
 return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | API Token Hash
+    |--------------------------------------------------------------------------
+    |
+    | Base64-encoded bcrypt hash of the API token used for authentication.
+    | Generate it with: php artisan clavis:token
+    |
+    | This value must be kept secret, similar to APP_KEY.
+    |
+    */
+
     'hash' => env('CLAVIS_HASH'),
+
 ];
