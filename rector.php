8e7d2cea38d3d8d1eb0e0b8103d2239a2c39a1c9
Siarhei Kulich <siarhei.kulich@gmail.com>
2026-04-02 16:55:30 +0200

    Enable `codingStyle` set in `rector.php` configuration

--- rector.php
+++ rector.php
@@ -13,8 +13,8 @@ return RectorConfig::configure()
     ->withPreparedSets(
         deadCode: true,
         codeQuality: true,
+        codingStyle: true,
         typeDeclarations: true,
         privatization: true,
         earlyReturn: true,
-        codingStyle: true,
     );
